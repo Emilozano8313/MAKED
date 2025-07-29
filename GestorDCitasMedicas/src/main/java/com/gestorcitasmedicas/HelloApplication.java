8796092457ed63e528382fcb80c2b9837666fba2
Diego <20243ds148@utez.edu.mx>
2025-07-29 16:44:42 -0600

    Cambios Kevin

--- GestorDCitasMedicas/src/main/java/com/gestorcitasmedicas/HelloApplication.java
+++ GestorDCitasMedicas/src/main/java/com/gestorcitasmedicas/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/gestorcitasmedicas/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 759 , 422);
         stage.setTitle("Hello world!");
         stage.setScene(scene);
         stage.show();
