e4addb5b0712cf7f31e2d663fc1f64cd38ec811f
Emilozano8313 <20243ds174@utez.edu.mx>
2025-08-15 02:45:09 -0600

    Eliminación de la definición redundante del método main en la clase App y ajuste en la llamada a launch().

--- GestorDCitasMedicas/src/main/java/com/gestorcitasmedicas/App.java
+++ GestorDCitasMedicas/src/main/java/com/gestorcitasmedicas/App.java
@@ -25,10 +25,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
-    }
-
-    public static void main(String[] args) {
         try {
             launch();
         } catch (Exception e) {
